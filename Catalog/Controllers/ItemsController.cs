4734716ac88a68208bd5a787affcf5faf102f892
srkans <serkansacma@hotmail.com>
2022-12-22 14:31:29 +0300

    added restapi put(update) item method

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -56,6 +56,26 @@ namespace Catalog.Controllers
             return CreatedAtAction(nameof(GetItem),
              new{ id = item.Id}, item.AsDto());
         }
+        //PUT /items/ {id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if(existingItem is null)
+            {
+                return NotFound();
+            }
+
+            Item updatedItem = existingItem with {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+
+            repository.UpdateItem(updatedItem);
+
+            return NoContent();
+        }
 
     }
     
--- Catalog/Repositories/InMemItemsRepository.cs
+++ Catalog/Repositories/InMemItemsRepository.cs
@@ -29,6 +29,12 @@ namespace Catalog.Repositories
         {
             items.Add(item);
         }
+
+        public void UpdateItem(Item item)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == item.Id );
+            items[index] = item;
+        }
     }
 
 
