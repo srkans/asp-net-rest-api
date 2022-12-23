6bdd7ef09c5f477c93b94f02617357387430ddb3
srkans <serkansacma@hotmail.com>
2022-12-23 07:41:45 +0300

    changed IItemsRepository for asynchronous

--- Catalog/Repositories/InMemItemsRepository.cs
+++ Catalog/Repositories/InMemItemsRepository.cs
@@ -15,28 +15,28 @@ namespace Catalog.Repositories
          };
 
    
-        public IEnumerable<Item> GetItems()
+        public IEnumerable<Item> GetItemsAsync()
         {
             return items;
         }
 
-        public Item GetItem(Guid id)
+        public Item GetItemAsync(Guid id)
         {
             return items.Where(item => item.Id == id).SingleOrDefault();
         }
 
-        public void CreateItem(Item item)
+        public void CreateItemAsync(Item item)
         {
             items.Add(item);
         }
 
-        public void UpdateItem(Item item)
+        public void UpdateItemAsync(Item item)
         {
             var index = items.FindIndex(existingItem => existingItem.Id == item.Id );
             items[index] = item;
         }
 
-        public void DeleteItem(Guid id)
+        public void DeleteItemAsync(Guid id)
         {
          var index = items.FindIndex(existingItem => existingItem.Id == id );
          items.RemoveAt(index);
